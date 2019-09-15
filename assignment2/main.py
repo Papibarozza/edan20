05a3463f92bd892d366ddbfd5503eba7fbdb5c0c
Arvid Mildner <mildner.arvid@gmail.com>
2019-09-15 20:01:02 +0200

    Working according to lab specs

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,5 +1,8 @@
 import regex as re
+import copy
 from math import log
+import sys
+from collections import Counter
 def normalize_file(file='Selma.txt'):
   with open(file, 'r',encoding="utf-8") as file:
     text = file.read()
@@ -60,21 +63,106 @@ def count_bigrams(words):
         else:
             frequency_bigrams[bigram] = 1
     return frequency_bigrams
+def good_turing(bigrams):
+    """Calculates the GT frequencies for the bigram occurences and writes it back to the bigram dictionary
+
+        Parameters:
+            argument1 {String : int }: Bigram dictionary
+    """
+
+    k = 6 #Calculate GT for occurences [0<occurences<=k]
+    c_stars = [0]*(k+1)
+    #Calculate GT estimates and store them
+    for c in range(1,k+1):
+        N_c = sum(map((c).__eq__, bigrams.values()))
+        N_cplus1= sum(map((c+1).__eq__, bigrams.values()))
+        c_star = (c+1)*N_cplus1/N_c
+        c_stars[c]=c_star
+    
+    #Write the GT frequencies back to the bigram
+    for c in range(1,k+1):
+        for bigram in bigrams:
+            if(bigrams[bigram] == c):
+                bigrams[bigram]=c_stars[c]
+    
+    return bigrams
+
+    
+def P_katz(bigram,bigrams,bigramsGT,unigrams):
+    w1 = bigram[0]
+    w2 = bigram[1]
+    nr_words = sum(unigrams.values())
+    nr_bigrams = sum(bigrams.values())
+    nr_items = nr_words+nr_bigrams
+    if(bigram in bigramsGT):
+        return bigrams[bigram]/unigrams[w1] #how he wants it
+        #return bigrams[bigram]/nr_items #what it should be?
+    else:
+        #print('Using backoff..')
+        #alpha = calculate_alpha2(w1,w2,nr_items,nr_words,bigramsGT,unigrams)
+        #return alpha * unigrams[w2]/nr_items #what it should be?
+        return unigrams[w2]/nr_words #How he wants it.
+
+
+def calculate_alpha2(w1,w2,nr_items,nr_words,bigramsGT,unigrams):
+    nominator= 1
+    denominator =1
+    for ngram_pair,value in bigramsGT.items():
+        if(ngram_pair[0]==w1):
+            #nominator-=value/unigrams[w1] #Wiggly probability
+            nominator-=value/(nr_items)
+            denominator-=unigrams[ngram_pair[1]]/nr_words
+    return nominator/denominator
+
+
+def most_probable_bigram(word,bigram,bigrams_GT_estimate,unigrams):
+    
+    prob_old = 0
+    wrd =''
+    k = Counter(unigrams)
+    for wrd in k.most_common(100):
+        prob = P_katz((word,wrd[0]),bigrams,bigrams_GT_estimate,unigrams)
+        if(prob > prob_old and wrd[0] != '<s>' and wrd[0] !='</s>' ):
+            prob_old=prob
+            best = wrd[0]
+            print(best)
+    return best
+        
+
+
+
 
 if __name__ == "__main__":
     words = tokenize(normalize_file('Selma.txt'))
     unigrams = count_unigrams(words)
     bigrams = count_bigrams(words)
+    bigrams_GT_estimate = good_turing(copy.deepcopy(bigrams)) #Recalculate
+    assert not(bigrams == bigrams_GT_estimate),'Shouldnt be equal'
+    bigram=('det','är')
+    while True:
+        word = input("-: ")
+        print(most_probable_bigram(word,bigrams,bigrams_GT_estimate,unigrams))
+
+    #print(bigram,P_katz(bigram,bigrams,bigrams_GT_estimate,unigrams))
+    """ sentence = '<s> det var en gång en katt som hette nils </s>'
+    sentence_words = tokenize(sentence)
+    bigram_sentence = count_bigrams(tokenize(sentence))
+    for bigram in bigram_sentence:
+        print(bigram,P_katz(bigram,bigrams,bigrams_GT_estimate,unigrams)) """
+
+    #print(bigrams[bigram])
+
+"""     
     lr = likelihood_ratio(words,unigrams,bigrams)
     sentence = '</s> det var en gång en katt som hette nils </s>'
     sentence_words = tokenize(sentence)
     bigram_sentence = count_bigrams(tokenize(sentence))
     for word in sentence_words:
-      print(word,unigrams[word],len(words),unigrams[word]/len(words))
+      print(word,unigrams[word],len(words),unigrams[word]/len(words)) """
 
     #for bigram in bigram_sentence:
    
-    print(('stilla','på'), lr[('stilla','på')])
+   # print(('stilla','på'), lr[('stilla','på')])
       
 """     i = 0
     for word in sorted(unigrams.keys(), key=unigrams.get, reverse=True):
